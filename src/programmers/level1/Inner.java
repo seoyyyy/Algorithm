88720bf098ce56c92239b6ae626ea0fb8471a032
seoyyyy <seoyyyy@naver.com>
2021-04-24 11:32:56 +0900

    내적

--- src/programmers/level1/Inner.java
+++ src/programmers/level1/Inner.java
@@ -1,13 +1,13 @@
 package programmers.level1;
 // 내적
 public class Inner {
-        public int solution(int[] a, int[] b) {
-            int answer = 0;
-            for(int i=0; i<a.length; i++){
-                answer += (a[i]*b[i]);
-            }
-            return answer;
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i=0; i<a.length; i++){
+            answer += (a[i]*b[i]);
         }
+        return answer;
+    }
 
     public static void main(String[] args){
         Inner inner = new Inner();
