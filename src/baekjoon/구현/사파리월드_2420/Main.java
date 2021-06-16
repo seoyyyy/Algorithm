d77b978e1b70fe1c7dfafb1c64f52842b57ee1b4
seoyyyy <seoyyyy@naver.com>
2021-06-16 23:51:07 +0900

    사파리월드_2420

---
+++ src/baekjoon/구현/사파리월드_2420/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.구현.사파리월드_2420;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Long a = Long.parseLong(st.nextToken());
+        Long b = Long.parseLong(st.nextToken());
+        System.out.println(Math.abs(a-b));
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        try {
+            m.solution();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
